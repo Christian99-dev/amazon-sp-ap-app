8f9accd28d1d35be5ff0a22720d36e9dce9b0507
Christian Keller <k.christian9@web.de>
2024-03-29 07:23:26 +0100

    warnings

--- app/src/components/sections/pricing/PricingTable.tsx
+++ app/src/components/sections/pricing/PricingTable.tsx
@@ -7,15 +7,15 @@ const PricingTable = ({
 }) => {
   return (
     <table className="flex w-full h-full">
-      {listingOffers.map((item) => (
-        <div className="flex-1 h-full text-center even:bg-slate-100">
+      {listingOffers.map((item, index) => (
+        <div key={index} className="flex-1 h-full text-center even:bg-slate-100">
           <img
             className="object-fill h-20 p-3 m-auto"
             src={`/flags/${item.countryCode}.svg`}
           ></img>
 
-          {item.prices.map((price) => (
-            <div className="py-4 text-xs">
+          {item.prices.map((price, index) => (
+            <div key={index} className="py-4 text-xs">
               {price} <span className="text-[10px]"> {item.CurrencyCode}</span>
             </div>
           ))}
