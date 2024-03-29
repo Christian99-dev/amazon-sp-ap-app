fe222d8e784b3e31c564330fdbd0ce4e1178184e
Christian Keller <k.christian9@web.de>
2024-03-29 07:34:27 +0100

    style: button

--- app/src/components/shared/Button.tsx
+++ app/src/components/shared/Button.tsx
@@ -18,7 +18,7 @@ const Button = ({
         disabled
           ? "bg-blue-300 cursor-not-allowed"
           : "bg-blue-600 hover:bg-blue-800"
-      } font-medium text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center ${
+      } font-medium text-center me-2  inline-flex items-center ${
         size === "sm"
           ? "text-xs px-2 py-1.5 rounded-md"
           : "text-sm px-5 py-2 rounded-lg"
