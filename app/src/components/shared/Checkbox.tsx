a8b2d64f05698580f385cb0711d4c8c76a03fb77
Christian Keller <k.christian9@web.de>
2024-04-20 13:45:05 +0200

    checkbox hover cursor

--- app/src/components/shared/Checkbox.tsx
+++ app/src/components/shared/Checkbox.tsx
@@ -18,11 +18,11 @@ const Checkbox = ({
         type="checkbox"
         checked={checked}
         onChange={onChange}
-        className="w-3 h-3 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        className="w-3 h-3 text-blue-600 bg-gray-100 border-gray-300 rounded cursor-pointer  focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
       />
       <label
         htmlFor={htmlFor}
-        className="text-xs font-medium text-gray-900 ms-2 dark:text-gray-300"
+        className="text-xs font-medium text-gray-900 cursor-pointer  ms-2 dark:text-gray-300"
       >
         {text}
       </label>
